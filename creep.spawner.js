adc7534203599e52f4d46b7473e1ade236594591
Sy Harger <silasharger@gmail.com>
2018-07-30 17:30:52 +0200

    spawn more creeps

--- creep.spawner.js
+++ creep.spawner.js
@@ -1,14 +1,26 @@
 const creepSpawner = {
   run() {
-    if (Object.keys(Game.creeps).length < 2) {
+    if (Object.keys(Game.creeps).length < 8) {
       console.log('we have less than two creeps')
 
       Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1', {
         memory: { role: 'harvester' },
       })
 
+      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester2', {
+        memory: { role: 'harvester' },
+      })
+
       Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Upgrader1', {
         memory: { role: 'upgrader' },
+      })
+
+      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Upgrader2', {
+        memory: { role: 'upgrader' },
+      })
+
+      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Builder1', {
+        memory: { role: 'builder' },
       })
     }
   },
